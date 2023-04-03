9ecc104432a77689e480f17409827095c48feeee
Ketan Patel <k2patel@duck.com>
2023-04-03 16:15:12 -0400

    mw-dictionary

---
+++ mw-dictionary/README.md
@@ -0,0 +1,11 @@
+we use the os.Getenv() function to retrieve the API key from the MERRIAM_WEBSTER_API_KEY environment variable. You can set this variable in your environment by running a command like 
+
+`export MERRIAM_WEBSTER_API_KEY=<YOUR_API_KEY_HERE>`
+
+This implementation uses the Merriam-Webster API to obtain the definition of the user-specified word, formats the response to include the word, pronunciation, part of speech, and definition, and prints the formatted response to the console. The API key needs to be replaced with an actual API key obtained from the Merriam-Webster developer portal. The tool can be run from the command line by passing a word to be queried as a command-line argument.
+
+`go run dictionary.go <word>`
+
+In this workflow, we define a job named build that runs on the ubuntu-latest operating system. The steps in the job check out the repository, set up the Go environment, build the dictionary binary, and create an artifact from the binary using the actions/upload-artifact action.
+
+Now, when `push` changes to the repository, the GitHub Actions workflow will automatically run and create an artifact from the built dictionary binary. You can view and download the artifact by going to the Actions tab in the repository and selecting the latest workflow run.
---
+++ mw-dictionary/dictionary.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type definition struct {
+	Dt       string   `json:"dt"`
+	Fl       string   `json:"fl"`
+	Shortdef []string `json:"shortdef"`
+}
+
+type pronunciation struct {
+	Mw string `json:"mw"`
+}
+
+type entry struct {
+	Hwi struct {
+		Prs []pronunciation `json:"prs"`
+	} `json:"hwi"`
+	Fl string `json:"fl"`
+}
+
+func getDefinition(word string, apiKey string) (string, error) {
+	url := fmt.Sprintf("https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s", word, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var definitions []definition
+	err = json.NewDecoder(resp.Body).Decode(&definitions)
+	if err != nil {
+		return "", err
+	}
+
+	// Check if the API returned a definition
+	if len(definitions) > 0 {
+		definition := definitions[0]
+		return definition.Shortdef[0], nil
+	}
+
+	return "", fmt.Errorf("no definition found for %s", word)
+}
+
+func formatDefinition(pronunciation string, partOfSpeech string, definition string) string {
+	formattedDefinition := fmt.Sprintf("`%s`\t (%s): %s", pronunciation, partOfSpeech, definition)
+	return formattedDefinition
+}
+
+func main() {
+
+	apiKey := os.Getenv("MERRIAM_WEBSTER_API_KEY")
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run dictionary.go <word>")
+		return
+	}
+
+	word := os.Args[1]
+	definition, err := getDefinition(word, apiKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	url := fmt.Sprintf("https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s", word, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var entries []entry
+	err = json.NewDecoder(resp.Body).Decode(&entries)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if len(entries) > 0 {
+		pronunciation := entries[0].Hwi.Prs[0].Mw
+		partOfSpeech := entries[0].Fl
+		formattedDefinition := formatDefinition(pronunciation, partOfSpeech, definition)
+		fmt.Println(formattedDefinition)
+	} else {
+		fmt.Printf("No definition found for %s\n", word)
+	}
+}
